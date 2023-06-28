0b03dc4d9d4d3945c8e6200611f2e543fabb043e
Anushka Tyagi <anushka.tyagi161998@gmail.com>
2023-06-28 17:46:21 +0530

    new screenshot.php

--- screenshot.php
+++ screenshot.php
@@ -2,13 +2,13 @@
 require_once 'vendor/autoload.php';
 
 use Symfony\Component\Panther\Client;
+use Symfony\Component\Panther\DomCrawler\Crawler;
 use Facebook\WebDriver\WebDriverDimension;
 
 $url = 'https://epaper.navgujaratsamay.com/3725080/Ahmedabad#page/5/3';
 $outputFile = 'screenshot.png';
 
 $client = Client::createChromeClient();
-
 $client->start();
 $client->request('GET', $url);
 
@@ -23,17 +23,10 @@ $client->executeScript('window.scrollTo(1000, 0);');
 $window = $client->getWebDriver()->manage()->window();
 $scrollWidth = $client->executeScript('return Math.max(document.documentElement.scrollWidth, document.body.scrollWidth);');
 $scrollHeight = $client->executeScript('return Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);');
-sleep(3);
 $window->setSize(new WebDriverDimension($scrollWidth, $scrollHeight));
 
-try {
-    $client->wait(5, 500)->until(function () use ($client) {
-        $element = $client->getCrawler()->filter('.doesnt_exist')->first();
-    });
-} catch (Exception $e) {
-    echo PHP_EOL . $e . PHP_EOL;
-} finally {
-    $client->takeScreenshot($outputFile);
-    $window->setSize(new WebDriverDimension(1920, 1080));
-    $client->quit();
-}
+usleep(3000000); // Wait for 3 seconds (adjust the sleep time as needed)
+
+$client->takeScreenshot($outputFile);
+$window->setSize(new WebDriverDimension(1920, 1080));
+$client->quit();
