22e38776be35147bd316d93659a3258018ed6f31
Anushka Tyagi <anushka.tyagi161998@gmail.com>
2023-07-14 19:02:49 +0530

    template for website testing

--- test.php
+++ test.php
@@ -1,26 +1,27 @@
 <?php
-// $ch = curl_init();
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// curl_setopt($ch, CURLOPT_URL, "https://epaper.thehindu.com/ccidist-replica-reader/?epub=https://epaper.thehindu.com/ccidist-ws/th/th_delhi/issues/42353/#/pages/1");
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13");
-// $data = curl_exec($ch);
-// curl_close($ch);
 
 
-function getdata($url)
+function convertHindiToEnglishNumber($hindiNumber)
 {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false); // Disable redirection
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept-Language: en-US,en;q=0.5', 'Accept-Encoding: gzip, deflate', 'Connection: keep-alive', 'Upgrade-Insecure-Requests: 1', 'TE: Trailers'));
-    $data = curl_exec($ch);
-    curl_close($ch);
-    return $data;
+    $hindiToEnglishMap = array(
+        '०' => '0',
+        '१' => '1',
+        '२' => '2',
+        '३' => '3',
+        '४' => '4',
+        '५' => '5',
+        '६' => '6',
+        '७' => '7',
+        '८' => '8',
+        '९' => '9'
+    );
+
+    $englishNumber = strtr($hindiNumber, $hindiToEnglishMap);
+
+    return $englishNumber;
 }
 
-$url = 'https://epaper.thehindu.com/ccidist-ws/th/th_bangalore/issues/42323/OPS/G7HBEMLLT.1%2BG7HBEMLLT.1_background-2048.png';
-$data = getdata($url);
-echo $data;
+$hindiNumber = '१२३४५६७८९०';
+$englishNumber = convertHindiToEnglishNumber($hindiNumber);
+
+echo $englishNumber; // Output: 1234567890
